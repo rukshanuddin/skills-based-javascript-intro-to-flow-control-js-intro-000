029427cc2bf28c069c318c447686c7a27336640f
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-16 01:37:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,12 @@
-function basicTeenager(age) { if (age > 13 && age < 19)
+function basicTeenager(age) { if age
  return `You are a teenager!`
 }
 
-function teenager(age) {
+function teenager(age) {if (age > 13 && age < 19 ) {
+  return `You are a teenager!`
+} else {
+  return `You are not a teenager!`// executed if `conditionToTest` is falsey
+}
 
 }
 
