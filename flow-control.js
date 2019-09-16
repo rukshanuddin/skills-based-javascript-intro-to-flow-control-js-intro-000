0b73570e7ab0b1604a607d158615260517c73808
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-16 02:02:41 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -21,9 +21,24 @@ function ageChecker(age) {if (age > 12 && age <= 20 ) {
 } else { return `You are a grownup`} }
 
 function ternaryTeenager(age) {
-
+  return (age = age > 12 && age < 19 ? 'You are a teenager' : 'You are not a teenager');
 }
 
 function switchAge(age) {
-
+  case 13:
+    return 'You are a teenager';
+  case 14:
+    return 'You are a teenager';
+  case 15:
+    return 'You are a teenager';
+  case 16:
+    return 'You are a teenager';
+  case 17:
+    return 'You are a teenager';
+  case 18:
+    return 'You are a teenager';
+  case 19:
+    return 'You are a teenager';
+  default:
+    return 'You have an age'
 }
