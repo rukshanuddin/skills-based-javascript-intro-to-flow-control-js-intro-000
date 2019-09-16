027e8d816b98214713f395b5eab1238bf2bad046
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-16 02:04:53 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,7 +25,8 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  case 13:
+  switch(age)
+  {case 13:
     return 'You are a teenager';
   case 14:
     return 'You are a teenager';
@@ -40,5 +41,5 @@ function switchAge(age) {
   case 19:
     return 'You are a teenager';
   default:
-    return 'You have an age';
+    return 'You have an age';}
 }
