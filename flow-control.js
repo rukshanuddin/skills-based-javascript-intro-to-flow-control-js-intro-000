73826e5cb3e85be5a29632ca28ee9b1a932e3c48
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-16 01:55:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,9 @@
-function basicTeenager(age) {if (age <= 19 && age => 13){
-      return "You are a teenager!";
-    }
+function basicTeenager(age) {
+
+  if (age > 12 && age < 20) {
+    return 'You are a teenager!';
+  }
+
 }
 
 function teenager(age) {if (age => 13 && age <= 19 ) {
